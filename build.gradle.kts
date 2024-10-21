787690de4a2dde305c96e439561a16794e56e9cc
yeseong <kwonyebail@steppay.kr>
2024-10-21 15:52:43 +0900

    feat: 환경별 설정 분리 및 필요한 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
     id("org.springframework.boot") version "3.3.4"
     id("io.spring.dependency-management") version "1.1.6"
     id("org.jmailen.kotlinter") version "4.4.1"
 
-    val kotlinVersion = "2.0.0"
+    val kotlinVersion = "1.9.25"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
@@ -31,17 +29,27 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Postgresql
+    runtimeOnly("org.postgresql:postgresql")
 
     // QueryDSL
     val querydslVersion = "5.0.0"
     implementation("com.querydsl:querydsl-jpa:$querydslVersion:jakarta")
     kapt("com.querydsl:querydsl-apt:$querydslVersion:jakarta")
 
-    // Postgresql
-    runtimeOnly("org.postgresql:postgresql")
-}
+    // Actuator
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
+    // Coroutine
+    val coroutineVersion = "1.9.0"
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutineVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor:${coroutineVersion}")
+
+    /** Test **/
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("com.h2database:h2")
+}
 
 kotlin {
     compilerOptions {
