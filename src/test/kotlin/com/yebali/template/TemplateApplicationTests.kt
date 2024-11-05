0a6dc33694e0bae9ca62b272b8191e00acf7f3fd
yeseong <kwonyebail@steppay.kr>
2024-11-05 18:45:24 +0900

    feat: add spring boot test support module

--- src/test/kotlin/com/yebali/template/TemplateApplicationTests.kt
+++ src/test/kotlin/com/yebali/template/TemplateApplicationTests.kt
@@ -1,12 +1,8 @@
 package com.yebali.template
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
-@SpringBootTest
-@ActiveProfiles("test")
-class TemplateApplicationTests {
+class TemplateApplicationTests : SpringBootTestSupport() {
     @Test
     fun contextLoads() {
     }
